dcd6423e4d6a155782a1c1d61f30d579071cf255
Diana Tavolzhan <di.tavolzhan@gmail.com>
2025-03-28 15:43:48 +0200

    update read_population_data method

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,17 +1,31 @@
 import csv
+import os
+
 
 def read_population_data(file_path):
     """Читання даних з файлу і повернення списку країн з їх площею та населенням"""
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Файл за вказаним шляхом {file_path} не знайдено.")
+
     data = []
-    with open(file_path, 'r', encoding='utf-16') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            country, area, population = row
-            data.append({
-                "country": country,
-                "area": float(area),
-                "population": int(population)
-            })
+    try:
+        with open(file_path, 'r', encoding='utf-16') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if len(row) != 3:
+                    raise ValueError(f"Невірний формат рядка: {row}")
+                country, area, population = row
+                try:
+                    data.append({
+                        "country": country,
+                        "area": float(area),
+                        "population": int(population)
+                    })
+                except ValueError:
+                    raise ValueError(f"Невірний формат чисел у рядку: {row}")
+    except Exception as e:
+        raise e
+
     return data
 
 def sort_by_area(data):
