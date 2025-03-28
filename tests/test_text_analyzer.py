d2cabdd76361c77243bf2314f95a9bc2195cc430
Diana Tavolzhan <di.tavolzhan@gmail.com>
2025-03-28 15:48:50 +0200

    unit test for read_population_data

--- tests/test_text_analyzer.py
+++ tests/test_text_analyzer.py
@@ -1,5 +1,5 @@
 import pytest
-from text_analyzer import  sort_by_area, sort_by_population
+from text_analyzer import read_population_data, sort_by_area, sort_by_population
 
 # Фікстура для тестових даних
 @pytest.fixture
@@ -47,3 +47,22 @@ def test_sort_by_area(data, expected):
 ])
 def test_sort_by_population(data, expected):
     assert sort_by_population(data) == expected
+
+
+# Тестування функції читання файлу для правильного формату даних
+def test_read_population_data_valid():
+    # Створюємо тимчасовий файл для тестування
+    test_file = "test_population_data.txt"
+    with open(test_file, "w", encoding="utf-16") as file:
+        file.write("Україна, 603628, 41300000\n")
+        file.write("Канада, 9984670, 38005238\n")
+
+    result = read_population_data(test_file)
+
+    assert len(result) == 2  # Перевірка, що дані зчитані правильно
+    assert result[0]["country"] == "Україна"  # Перевірка країни
+    assert result[1]["area"] == 9984670  # Перевірка площі
+
+    # Видалити тимчасовий файл після тесту
+    import os
+    os.remove(test_file)
